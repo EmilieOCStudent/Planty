c14dcf1eeaf1f6cc329b35085a68ad925445b50b
Emilie Lenoble <mimi.lnb98@gmail.com>
2024-08-26 14:49:51 +0200

    pages 1 et 2

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -25,5 +25,19 @@ function your_theme_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'your_theme_enqueue_styles');
 
 /*  Add your own functions below this line.
-    ======================================== */ 
+    ======================================== */
+    function planty_display_can_shortcode() {
+    ob_start(); ?>
+        <div class="can-container">
+            <img src="<?php echo get_stylesheet_directory_uri();  ?>/images_planty/canette.png" alt="Planty Energy Drink" class="can">
+            <div>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images_planty/gauche.png" alt="Gauche branches" class="branches gauche">
+                <img src="<?php echo get_stylesheet_directory_uri();  ?>/images_planty/droite.png" alt="Droite branches" class="branches droite">
+            </div>
+        </div>
+        <?php
+    return ob_get_clean();
+}
+add_shortcode('planty_display_can', 'planty_display_can_shortcode');
+?>
 
